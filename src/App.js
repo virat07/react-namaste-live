34ce080f7ecf0a3cd5317333385c99aba3384c69
Bharat Gupta <abond0707@gmail.com>
2023-01-09 02:16:36 -0800

    Lets get hooked!

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Header from "./components/Header";
+import Body from "./components/Body";
+import Footer from "./components/Footer";
+/**
+     Header
+        - Logo(Title)
+        - Nav Items(Right Side)
+        - Cart
+     Body 
+        - Search bar
+        - RestrauntList
+          - RestaurantCard (many cards)
+              - Image
+              - Name
+              - Rating
+              - Cusines
+     Footer
+      - links
+      - Copyright
+    
+    */
+
+// Composing Comopnentss
+// NAMED EXPORT import is nothing but when we export a component without default keyword we use { } in the import
+// whereas when we export using DEFAULT keywoard then we use just the component name! Also remember we can't export 2 components in default
+
+// import { ComponentName } from './ component/' where { } doesn't means object destructing but it means 
+//Config Driven UI
+
+
+
+
+// no key (not acceptable)<<<<<<<<<<< index key(last option) <<<<< unquie key (best practice)
+
+
+const AppLayout = () => {
+  return (
+    <>
+      <Header />
+      <Body />
+      <Footer />
+    </>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<AppLayout />);
---
+++ src/components/Body.js
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import { restrautList } from "./config";
+import { RestrauntCard } from "./RestrauntCard";
+
+// hooks is a normal functions
+
+export default Body = () => {
+  //searchText is a local variable
+  // To create state variable
+  const [searchText, setSearchText] = useState("");
+  const [restaurants, setRestaurants] = useState(restrautList);
+  //why state variables?- 
+  const filterData = (searchText, restaurants)=>{
+    if(searchText)
+    return restrautList.filter((restaurant)=>restaurant.data.name.includes(searchText));
+    else 
+    return restrautList;
+
+  }
+  return (
+    <>
+      <div className="search-container">
+        <input
+          type="text"
+          className="search-input "
+          placeholder="Search"
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+        />
+        <button className="search-btn"
+        onClick={()=>{
+          const data = filterData(searchText,restaurants);
+
+          setRestaurants(data);
+        }}
+        >Search</button>
+      </div>
+      <div className="restaurant-list">
+        {restaurants.map((restaurant) => {
+          return (
+            <RestrauntCard {...restaurant.data} key={restaurant.data.id} />
+          );
+        })}
+      </div>
+    </>
+  );
+};
